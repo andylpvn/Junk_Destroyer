4bb9903c69548da8c5fdeed59625bdfa9cae02b0
jacobstack <jacobwstack12@gmail.com>
2022-04-07 16:53:37 -0600

    running executables in background
    it is only changing so many files because the project location on my PC has changed.

--- UWPApp/UWPApp/UWPApp/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ UWPApp/UWPApp/UWPApp/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace UWPApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/UWPApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UWPApp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
