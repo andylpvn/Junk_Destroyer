1a03ae60059cb1f2ecde8c34afbba4023493fa71
jacobstack <jacob.stack20@gmail.com>
2022-04-27 09:18:30 -0600

    Absolute paths initial push
    the CSV app lists are now created on c:/app-lists/
    
    the gui now uses absolute file paths.

--- UWPApp/apps_getInstalled.ps1
+++ UWPApp/apps_getInstalled.ps1
@@ -1,6 +1,9 @@
 # This PowerShell script creates two .csv files for storing information.
 # We use installed_names.csv for displaying information to the user, and installed_fullNames.csv for the deletion process.
 
+#create a new folder on the C: drive for our app lists. The C: drive allows us to access the lists from the GUI.
+New-Item "C:\app-lists" -Type Directory
+
 
 # 1. Retrieve all information about installed AppXPackages on the system
 Get-AppXPackage |`
@@ -9,12 +12,12 @@ Get-AppXPackage |`
 Select-Object -Property Name, PackageFullName |`
 
 # 3. Write full names file
-Export-Csv "$PSScriptRoot\installed_full.csv"
+Export-Csv "C:\app-lists\\installed_full.csv"
 
 # 4. Repeat steps for names file (only collect the names)
 Get-AppXPackage |`
 
 Select-Object -Property Name |`
 
-Export-Csv "$PSScriptRoot\installed_names.csv"
+Export-Csv "C:\app-lists\\installed_names.csv"
 
